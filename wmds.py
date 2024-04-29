9b03dc6d83de6e97ede150cc16aa640a923f4c6b
Maksim Shuklin <maximshuklin@mail.ru>
2024-04-29 16:06:46 +0300

    Add docstrings.

--- wmds.py
+++ wmds.py
@@ -20,7 +20,7 @@ class LearningRate:
 
 class WeightedMDS(BaseEstimator):
     """
-    Implementation of weighted multidimensional scaling to dimension 1
+    Weighted multidimensional scaling (to dimension 1).
     """
     def __init__(
         self,
@@ -29,6 +29,18 @@ class WeightedMDS(BaseEstimator):
         lr=1e-2,
         verbose=0
     ):
+        """
+        Parameters
+        ----------
+        max_iter : int, default=300
+            Maximum number of iterations of the weighted MDS algorithm.
+
+        weights : ndarray of shape (n_samples, n_samples)
+            Weights for wMDS.
+        
+        lr : float, default=1e-2
+            Learning rate for wMDS gradient descent.
+        """
         assert np.allclose(weights.T, weights), "Weights matrix should be symmetrical"
         assert np.all(weights >= 0), "Weights should be non-negative"
         self.weights = weights
@@ -38,16 +50,48 @@ class WeightedMDS(BaseEstimator):
         self.alpha = 0.9
         self.h = np.zeros(self.weights.shape[0])
 
-    def fit(self, dissimilarities):
-        self.dissimilarities = dissimilarities
+    def fit(self, X, y=None, init=None):
+        """
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_samples)
+            Dissimilarity matrix. 
+
+        y : Ignored
+            Not used, present for API consistency by convention.
+
+        init : ndarray of shape (n_samples,), default=None
+            Starting configuration of the `embedding` to initialize the wMDS
+            algorithm. By default, the algorithm is initialized with a randomly
+            chosen array.
+
+        Returns
+        -------
+        self : object
+            Fitted estimator.
+        """
+        self.dissimilarities = X
         if self.weights is None:
             self.weights = np.ones(self.dissimilarities.shape)
         return self
 
     def _compute_gradient(self, r):
+        """
+        Computes gradient of loss function with respect to r.
+
+        Parameters
+        ----------
+        r : ndarray of shape (n_samples,)
+            The vector in which the gradient must be calculated.
+
+        Returns:
+        -------
+        grad : ndarray of shape (n_samples,)
+            Gradient of loss with respect to r.
+        """
         n_samples = self.dissimilarities.shape[0]
         grad = np.zeros(n_samples)
-        # could be optimized
+        # Computations can be vectorized.
         for k in range(1, n_samples):
             grad[k] = 0.0
             for i in range(n_samples):
@@ -60,8 +104,15 @@ class WeightedMDS(BaseEstimator):
         return grad
 
     def predict(self):
+        """
+        Returns:
+        -------
+        r : ndarray of shape (n_samples,)
+            Result of wMDS algorithm.
+        """
         n_samples = self.dissimilarities.shape[0]
         r = np.random.rand(n_samples)
+        # Gradient descent with momentum
         for _ in range(self.max_iter):
             self.h = self.alpha * self.h + self.lr() * self._compute_gradient(r)
             r -= self.h
